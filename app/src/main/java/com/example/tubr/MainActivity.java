2d984b380b93c29ddef896a6b693ffbd9dc0c854
Ethan Millsone <millstone.ethan@gmail.com>
2019-02-17 16:17:36 -0500

    took out nav bar

--- app/src/main/java/com/example/tubr/MainActivity.java
+++ app/src/main/java/com/example/tubr/MainActivity.java
@@ -70,16 +70,6 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
