ae6d2e3d6d910f9a37a0880f449f9ed262675d39
Ethan Millsone <millstone.ethan@gmail.com>
2019-02-17 15:44:46 -0500

    tutor login and empty classeslist (wont actually have that functionality just needed a name)

---
+++ app/src/main/java/com/example/tubr/ClassesList.java
@@ -0,0 +1,13 @@
+package com.example.tubr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classes_list);
+    }
+}
---
+++ app/src/main/java/com/example/tubr/tutor_login.java
@@ -0,0 +1,33 @@
+package com.example.tubr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class tutor_login extends AppCompatActivity {
+
+    public final String TUTOR_NAME = "";
+    public final String TUTOR_CLASS = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_login);
+
+
+    }
+
+    public void go_to_classes(View view){
+        Intent intent = new Intent(this, ClassesList.class);
+        EditText tutor_name = (EditText) findViewById(R.id.editText2);
+        String message = tutor_name.getText().toString();
+        intent.putExtra(TUTOR_NAME,message);
+        EditText classes = (EditText) findViewById(R.id.editText2);
+        String message2 = tutor_name.getText().toString();
+        intent.putExtra(TUTOR_CLASS,message2);
+        startActivity(intent);
+    }
+}
